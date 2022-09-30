df6ef0411888f9c4b4a7bc996e44707ff380bb4e
MC-ALL <80183456+MC-ALL@users.noreply.github.com>
2022-09-30 23:44:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default Host TimeZONE IP
+sed -i 's/OpenWrt/RM2100/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
